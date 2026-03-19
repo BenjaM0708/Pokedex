03ab180ae5f72f4ef43e65ce4eb0853dfb7f83e5
Benja Muñoz <benjamunozja@gmail.com>
2026-03-20 00:52:34 +0100

    Section for buttons added in the html

--- call.js
+++ call.js
@@ -15,24 +15,19 @@ let rangePageNum = 0;
 const cardsContainer = document.getElementById("cards-container");
 cardsContainer.innerHTML = "";
 
-do {
+const nextPageButtom = async () => {
 
-const responseTestWhile = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${rangePageNum}&limit=20`);
-dataNextPage = await responseTestWhile.json();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${rangePageNum}&limit=20`);
+    dataNextPage = await response.json();
+    const pokemonArray = dataNextPage.results;
 
-const pokemonArray = dataNextPage.results;
+    for(let element of pokemonArray){
 
-for(let element of pokemonArray){
-const namePokemon = element.name;
-
-const pokemonDataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`);
-const pokemonData = await pokemonDataResponse.json();
-
-const weightPokemon = pokemonData.weight;
-pokeArrayData.push(`${namePokemon[0].toUpperCase()}${namePokemon.slice(1)} has a weight of ${weightPokemon}kg`);
-}//Fin del for
-
+        const namePokemon = element.name;
+        const pokemonDataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`);
+        const pokemonData = await pokemonDataResponse.json();
+        const weightPokemon = pokemonData.weight;
+        pokeArrayData.push(`${namePokemon[0].toUpperCase()}${namePokemon.slice(1)} has a weight of ${weightPokemon}kg`);
+    }
 rangePageNum += 20;
-} while(dataNextPage.next !== null);
-
-console.log(pokeArrayData);
+}
