96fb098637b15b18dd015436e390b20986513488
Benja Muñoz <benjamunozja@gmail.com>
2026-03-20 02:51:42 +0100

    Object Construction Started

--- call.js
+++ call.js
@@ -7,10 +7,9 @@ console.log(dataTest);
 //Global Variables
 
 const pokeArrayData = [];
-let dataNextPage = "init";
 let rangePageNum = 0;
 
-// Next Pokemon Page
+// Next Pokemon Page Function
 
 const cardsContainer = document.getElementById("cards-container");
 cardsContainer.innerHTML = "";
@@ -18,16 +17,41 @@ cardsContainer.innerHTML = "";
 const nextPageButtom = async () => {
 
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${rangePageNum}&limit=20`);
-    dataNextPage = await response.json();
-    const pokemonArray = dataNextPage.results;
-
-    for(let element of pokemonArray){
+    const dataNextPage = await response.json();
+    const pokemonBasicArray = dataNextPage.results;
+    
+    for(let element of pokemonBasicArray){
 
         const namePokemon = element.name;
         const pokemonDataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`);
-        const pokemonData = await pokemonDataResponse.json();
-        const weightPokemon = pokemonData.weight;
+        const pokemonData = await pokemonDataResponse.json(); //Pokemon Data Container - This Object has all information about a specific pokemon
+        
+        //Map for Abilities and Types
+
+        const abilitiesPokemonObject = pokemonData.abilities.map(
+            abilityPokemon => abilityPokemon.ability.name
+        );
+        
+        const typesPokemonObject = pokemonData.types.map(
+            typePokemon => `${typePokemon.type.name.toUpperCase()}${typePokemon.type.name.slice(1)}`
+        );
+
+        // Object Construction
+
+        const objectPokemon = {
+            //thumbnail: 'Media/016.png',
+            id: `${pokemonData.id}`,
+            name: pokemonData.name,
+            type: typesPokemonObject,
+            //description: 'A tough rock-bodied Pokémon that releases toxic minerals from its body when threatened.',
+            height: `${pokemonData.height} m`,
+            weight: `${pokemonData.weight} kg`,
+            ability: abilitiesPokemonObject,
+            //habitat: 
+        };
+
         pokeArrayData.push(`${namePokemon[0].toUpperCase()}${namePokemon.slice(1)} has a weight of ${weightPokemon}kg`);
+
     }
 rangePageNum += 20;
 }
