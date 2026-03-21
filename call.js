4a56b29c4a56f13b2a4b702e1b3ac220360196cb
Benja Muñoz <benjamunozja@gmail.com>
2026-03-21 12:12:21 +0100

    Changes in the structure

--- call.js
+++ call.js
@@ -9,12 +9,9 @@ console.log(dataTest);
 const pokeArrayData = [];
 let rangePageNum = 0;
 
-// Next Pokemon Page Function
+//Pokemon Array Builder
 
-const cardsContainer = document.getElementById("cards-container");
-cardsContainer.innerHTML = "";
-
-const nextPageButtom = async () => {
+//const pokeArrayBuilder = async () => {  BORRAR SIGNOS DE COMENTARIO LUEGO!!!!!!!!!!!!!!
 
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${rangePageNum}&limit=20`);
     const dataNextPage = await response.json();
@@ -24,35 +21,47 @@ const nextPageButtom = async () => {
 
         const namePokemon = element.name;
         const pokemonDataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`);
-        const pokemonData = await pokemonDataResponse.json(); //Pokemon Data Container - This Object has all information about a specific pokemon
+        const pokemonData = await pokemonDataResponse.json(); //Pokemon Data Container - This Object has all information about a specific pokemon. Remember that is a Iteration, so it's doing this kind of array for each pokemon
         
         //Map for Abilities and Types - Both are arrays
 
-        const abilitiesPokemonObject = pokemonData.abilities.map(
-            abilityPokemon => abilityPokemon.ability.name
+        const abilitiesPokemonArray = pokemonData.abilities.map(
+            abilityPokemon => `${abilityPokemon.ability.name[0].toUpperCase()}${abilityPokemon.ability.name.slice(1)}`
         );
-        
-        const typesPokemonObject = pokemonData.types.map(
-            typePokemon => `${typePokemon.type.name.toUpperCase()}${typePokemon.type.name.slice(1)}`
+
+        const typesPokemonArray = pokemonData.types.map(
+            typePokemon => `${typePokemon.type.name[0].toUpperCase()}${typePokemon.type.name.slice(1)}`
         );
 
         // Object Construction
 
         const objectPokemon = {
-            //thumbnail: 'Media/016.png',
-            id: `${pokemonData.id}`,
-            name: pokemonData.name,
-            type: typesPokemonObject,
+            thumbnail: pokemonData.sprites.front_default, 
+            id: pokemonData.id, 
+            name: `${pokemonData.name[0].toUpperCase()}${pokemonData.name.slice(1)}`, 
+            type: typesPokemonArray,
             //description: 'A tough rock-bodied Pokémon that releases toxic minerals from its body when threatened.',
             height: `${pokemonData.height} m`,
             weight: `${pokemonData.weight} kg`,
-            ability: abilitiesPokemonObject,
+            ability: abilitiesPokemonArray,
             //habitat: 
-        };
+        }; console.log(objectPokemon);
 
         //pokeArrayData.push(`${namePokemon[0].toUpperCase()}${namePokemon.slice(1)} has a weight of ${weightPokemon}kg`);
         //After finish the object for every pokemon, I need to push the objects in an array that it'll be the base to build every card in when the user changes of page
 
     }
 rangePageNum += 20;
-}
+//}                          BORRAR SIGNOS DE COMENTARIO LUEGO!!!!!!!!!!!!!!
+
+//Next Pokemon Page Function
+
+/*const cardsContainer = document.getElementById("cards-container");
+const nextButton = document.getElementById('next-button');
+
+const nextPageButtom =  () => {
+
+    //La función limpiará el contenedor HTML y desplegará las nuevas cartas con los pokemones correspondiesntes a la siguiente página de la API
+    cardsContainer.innerHTML = "";
+    //Call generateFunction - This function is using the array that "pokeArrayBuilder" does how a callback function
+}*/
